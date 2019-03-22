e68fa3c8df8b4d87c32f2026462836e2bc4ce13d
imanj12 <imanj12@gmail.com>
2019-03-22 15:22:33 -0400

    added search, refactored remove

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -3,77 +3,106 @@ class BinarySearchTree {
       this.root = null
    }
 
-   insertNode (value, left = null, right = null) {
-      // create node object - possibly use seperate class for this? Node class?
-      let node = {
-         value,
-         left,
-         right
-      }
+   // accepts value, returns true if insertion successful, false if not
+   insert (value) {
+      if (value.typeof === 'number') {
+         // create node object to be inserted
+         let newNode = {
+            value,
+            left: null,
+            right: null
+         }
 
-      let currentNode
-
-      if (this.root === null) {
-         this.root = node
-      } else {
-         currentNode = this.root
-         while (currentNode) {
-            if (value < currentNode.value) {
-               if (!currentNode.left) {
-                  currentNode.left = node
-                  break
+         if (this.root === null) {
+            // if tree is empty, insert as root node
+            this.root = newNode
+         } else {
+            // search for logical place for new node by BST rules: 
+            // traverse left if insert value is smaller, right if bigger, no duplicates, repeat until empty spot found
+            let currentNode = this.root
+            while (currentNode) {
+               if (value < currentNode.value) {
+                  if (currentNode.left === null) {
+                     currentNode.left = newNode
+                     return true
+                  } else {
+                     currentNode = currentNode.left
+                  }
+               } else if (value > currentNode.value) {
+                  if (currentNode.right === null) {
+                     currentNode.right = newNode
+                     return true
+                  } else {
+                     currentNode = currentNode.right
+                  }
                } else {
-                  currentNode = currentNode.left
+                  console.log('Node already exists - No duplicate nodes allowed')
+                  return false
                }
-            } else if (value > currentNode.value) {
-               if (!currentNode.right) {
-                  currentNode.right = node
-                  break
-               } else {
-                  currentNode = currentNode.right
-               }
-            } else {
-               console.log('Node already exists - No duplicate nodes allowed')
             }
          }
+      } else {
+         console.log('Insert failed - Can only insert numbers')
+         return false
       }
    }
 
-   removeNode (value) {
-      if (!this.root) {
+   // accepts value, returns null or found node object
+   search (value) {
+      if (this.root === null) {
          console.log('Tree is empty!')
+         return null
       } else {
          let currentNode = this.root
          while (currentNode) {
-            if (value < currentNode.value) {
+            if (value === currentNode.value) {
+               return currentNode
+            } else if (value < currentNode.value) {
                currentNode = currentNode.left
             } else if (value > currentNode.value) {
                currentNode = currentNode.right
-            } else {
-               if (currentNode.left === null && currentNode.right === null) {
-                  currentNode.value = null
-                  break
-               } else if (currentNode.left != null && currentNode.right === null) {
-                  currentNode.value = currentNode.left
-                  break
-               } else if (currentNode.left === null && currentNode.right != null) {
-                  currentNode.value = currentNode.right
-                  break
-               } else {
-                  minNodeValue = findMinNode(currentNode)
-                  removeNode(minNodeValue)
-                  currentNode.value = minNodevalue
-                  break
-               }
             }
          }
+         console.log("No node with that value!")
+         return null
       }
    }
 
-   // helper for removeNode - will return the value of the minimum node
+   // accepts value, searches by value, then removes found node object, returns true if successful, false if not
+   remove (value) {
+      let currentNode = searchNode(value)
+      if (currentNode) {
+         if (currentNode.left === null && currentNode.right === null) {
+            // node to be removed has no children
+            currentNode = null
+            return true
+         } else if (currentNode.left != null && currentNode.right === null) {
+            // has one left child branch, lifts this child branch up in place
+            currentNode.value = currentNode.left
+            return true
+         } else if (currentNode.left === null && currentNode.right != null) {
+            // has one right child, lifts this child branch up in place
+            currentNode.value = currentNode.right
+            return true
+         } else {
+            // has two children, finds minNode, removes it, then replaces currentNode value with minNode value
+            let minNodeValue = findMinNode(currentNode).value
+            removeNode(minNodeValue)
+            currentNode.value = minNodevalue
+            return true
+         }
+      } else {
+         return false
+      }
+   }
+
+   // helper for removeNode
+   // accepts node object, no return
    findMinNode (node) {
+      // always start search for min node from right branch of input node
+      node = node.right
       if (node.left === null) {
-         return node.value
+         return node
       } else {
          return findMinNode(node.left)
       }
