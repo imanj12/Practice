8aef3d95d3b5df8532db251f164a8954a5f83dfd
imanj12 <imanj12@gmail.com>
2019-03-22 16:01:14 -0400

    added comments, fixed bug with findMinNode because typed findMinnode

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -5,7 +5,7 @@ class BinarySearchTree {
 
    // accepts value, returns true if insertion successful, false if not
    insert (value) {
-      if (value.typeof === 'number') {
+      if (typeof value === 'number') {
          // create node object to be inserted
          let newNode = {
             value,
@@ -16,6 +16,7 @@ class BinarySearchTree {
          if (this.root === null) {
             // if tree is empty, insert as root node
             this.root = newNode
+            console.log(`Inserted root node: ${this.root.value}`)
          } else {
             // search for logical place for new node by BST rules: 
             // traverse left if insert value is smaller, right if bigger, no duplicates, repeat until empty spot found
@@ -24,6 +25,7 @@ class BinarySearchTree {
                if (value < currentNode.value) {
                   if (currentNode.left === null) {
                      currentNode.left = newNode
+                     console.log(`Inserted: ${currentNode.left.value}`)
                      return true
                   } else {
                      currentNode = currentNode.left
@@ -31,18 +33,19 @@ class BinarySearchTree {
                } else if (value > currentNode.value) {
                   if (currentNode.right === null) {
                      currentNode.right = newNode
+                     console.log(`Inserted: ${currentNode.right.value}`)
                      return true
                   } else {
                      currentNode = currentNode.right
                   }
                } else {
-                  console.log('Node already exists - No duplicate nodes allowed')
+                  console.log('Insert failed - No duplicate nodes allowed')
                   return false
                }
             }
          }
       } else {
-         console.log('Insert failed - Can only insert numbers')
+         console.log('Insert failed: NaN')
          return false
       }
    }
@@ -56,6 +59,7 @@ class BinarySearchTree {
          let currentNode = this.root
          while (currentNode) {
             if (value === currentNode.value) {
+               console.log(`Search found: ${currentNode.value}`)
                return currentNode
             } else if (value < currentNode.value) {
                currentNode = currentNode.left
@@ -63,32 +67,37 @@ class BinarySearchTree {
                currentNode = currentNode.right
             }
          }
-         console.log("No node with that value!")
+         console.log(`Search empty: ${value}`)
          return null
       }
    }
 
    // accepts value, searches by value, then removes found node object, returns true if successful, false if not
    remove (value) {
-      let currentNode = searchNode(value)
+      let currentNode = this.search(value)
       if (currentNode) {
          if (currentNode.left === null && currentNode.right === null) {
             // node to be removed has no children
-            currentNode = null
+            currentNode.value = null
+            console.log(`Removed ${value}`)
             return true
          } else if (currentNode.left != null && currentNode.right === null) {
             // has one left child branch, lifts this child branch up in place
             currentNode.value = currentNode.left
+            console.log(`Replaced ${value} with ${currentNode.value}`)
             return true
          } else if (currentNode.left === null && currentNode.right != null) {
             // has one right child, lifts this child branch up in place
             currentNode.value = currentNode.right
+            console.log(`Replaced ${value} with ${currentNode.value}`)
             return true
          } else {
             // has two children, finds minNode, removes it, then replaces currentNode value with minNode value
-            let minNodeValue = findMinNode(currentNode).value
-            removeNode(minNodeValue)
-            currentNode.value = minNodevalue
+            // always start search for minNode from right branch
+            let minNodeValue = this.findMinNode(currentNode.right).value
+            this.remove(minNodeValue)
+            currentNode.value = minNodeValue
+            console.log(`Replaced ${value} with ${currentNode.value}`)
             return true
          }
       } else {
@@ -99,13 +108,24 @@ class BinarySearchTree {
    // helper for removeNode
    // accepts node object, no return
    findMinNode (node) {
-      // always start search for min node from right branch of input node
-      node = node.right
       if (node.left === null) {
          return node
       } else {
-         return findMinNode(node.left)
+         return this.findMinNode(node.left)
       }
    }
 
 }
+
+let BST = new BinarySearchTree()
+
+BST.insert('apple')
+BST.insert(10)
+BST.insert(5)
+BST.insert(15)
+BST.insert(20)
+BST.insert(13)
+BST.remove(10)
+BST.search(10)
+BST.remove(5)
+BST.search(5)
