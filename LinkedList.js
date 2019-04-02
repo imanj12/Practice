562924ecc5421f8a29ab2f8576eeba9f1216f829
imanj12 <imanj12@gmail.com>
2019-04-02 19:00:46 -0400

    added LinkedList class and created append and remove methods

---
+++ LinkedList.js
@@ -0,0 +1,62 @@
+class Node {
+   constructor(value) {
+      this.value = value
+      this.next = null
+      this.previous = null
+   }
+}
+
+class LinkedList {
+   constructor() {
+      this.head = null
+      this.tail = null
+      this.size = 0
+   }
+
+   printList() {
+      
+   }
+
+   append(value) {
+      const node = new Node(value)
+      if (this.head == null) {
+         this.head = node
+         this.tail = node
+      } else {
+         node.previous = this.tail
+         this.tail.next = node
+         this.tail = node
+         this.size++
+      }
+   }
+
+   remove(value) {
+      let currentNode = this.head
+      while (currentNode) {
+         if (currentNode.value === value) {
+            if (currentNode === this.head && currentNode === this.tail) {
+               this.head = null
+               this.tail = null
+            } else if (currentNode === this.head) {
+               this.head = this.head.next
+               this.head.previous = null
+            } else if (currentNode === this.tail) {
+               this.tail = this.tail.previous
+               this.tail.previous = null
+            } else {
+               currentNode.previous.next = currentNode.next
+               currentNode.next.previous = currentNode.previous
+               currentNode = null
+            }
+            this.size--
+            return true 
+         } else {
+            currentNode = currentNode.next
+         }
+      }
+      return false
+   }
+}
+
+const ll = new LinkedList()
+ll.insert(5)
