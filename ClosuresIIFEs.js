e643187d6f68398d27a6a804a57a73f938781133
imanj12 <imanj12@gmail.com>
2019-04-10 14:34:12 -0400

    simple closure

--- ClosuresIIFEs.js
+++ ClosuresIIFEs.js
@@ -1,3 +1,16 @@
+// simple closure
+function closure(first, last, age) {
+   let name = first + ' ' + last
+
+   return function () {
+      console.log(`Hello, my name is ${name} and I am ${age}.`)
+   }
+}
+
+let nameClosure = closure('Iman', 'Jawad', 29)
+nameClosure()
+
+
 // combines closure and IIFE
 var Counter = (function keepTrack() {
    let counter = 0
@@ -21,5 +34,4 @@ console.log(Counter.current())
 console.log(Counter.addOne())
 console.log(Counter.addOne())
 console.log(Counter.reset())
-
 
