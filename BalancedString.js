fe61fdb7fca1e2483332dffc472f9b770ec462fa
imanj12 <imanj12@gmail.com>
2019-04-10 13:47:14 -0400

    updated function to include args for opening and closing characters

--- BalancedString.js
+++ BalancedString.js
@@ -1,20 +1,20 @@
-function balancedString (str) {
-   let stack = []
-   let openers = ['(', '{', '[']
-   let closers = [')', '}', ']']
-   
-   for(let i=0;i<str.length;i++) {
-     let char = str.charAt(i)
-     if (openers.includes(char)) {
-       stack.push(char)
-     } else if (closers.includes(char)) {
-       if (openers.indexOf(stack.pop()) !== closers.indexOf(char)) {
-         return false
-       }
-     }
+function balancedString (str, openers, closers) {
+let stack = []
+
+for(let i=0;i<str.length;i++) {
+   let char = str.charAt(i)
+   if (openers.includes(char)) {
+      stack.push(char)
+   } else if (closers.includes(char)) {
+      if (openers.indexOf(stack.pop()) !== closers.indexOf(char)) {
+      return false
+      }
    }
-   return true
- }  
- 
- let string = '(a{a{aa]}))'
- console.log(balancedString(string))
+}
+return true
+}  
+
+let string = '(a{a{aa]}))'
+let openChars = ['(', '{', '[']
+let closeChars = [')', '}', ']']
+console.log(balancedString(string, openChars, closeChars))
